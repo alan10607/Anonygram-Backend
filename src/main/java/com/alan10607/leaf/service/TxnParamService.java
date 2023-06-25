747cae439cf772c1061dcd5dd8de49971dcabe81
alan10607 <hiden1414@gmail.com>
2023-06-26 03:14:11 +0800

    version 2.0.0

--- src/main/java/com/alan10607/leaf/service/TxnParamService.java
+++
@@ -1,8 +0,0 @@
-package com.alan10607.leaf.service;
-
-public interface TxnParamService {
-    String find(String key);
-    void update(String key, String value);
-    void update(String key, int value);
-    void delete(String key);
-}
---
+++ src/test/ab/DELETE:_forum_article_{id}_{no}.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ab_common.sh
+
+exit #Not support 2>&1 | tee "out/DELETE:_forum_article_{id}_{no}.$NOW.txt"
