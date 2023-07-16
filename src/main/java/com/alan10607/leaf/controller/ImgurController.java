bba7d3958fe9389f97d6c058d56fcea1c9e8aa2a
alan10607 <hiden1414@gmail.com>
2023-07-17 02:46:30 +0800

    redirect path

--- src/main/java/com/alan10607/leaf/controller/ImgurController.java
+++ src/main/java/com/alan10607/leaf/controller/ImgurController.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Map;
