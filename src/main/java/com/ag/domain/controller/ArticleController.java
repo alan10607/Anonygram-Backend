a07b627a85affadbbbe170bc104ed9f3b24ee345
alan10607 <hiden1414@gmail.com>
2024-02-29 02:15:00 +0800

    update retainFields

--- src/main/java/com/ag/domain/controller/ArticleController.java
+++ src/main/java/com/ag/domain/controller/ArticleController.java
@@ -1,16 +1,12 @@
 package com.ag.domain.controller;
 
+import com.ag.domain.constant.StatusType;
 import com.ag.domain.dto.ArticleDTO;
 import com.ag.domain.service.ArticleService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "article")
@@ -21,44 +17,55 @@ public class ArticleController {
 
     private final ArticleService articleService;
 
-    @GetMapping("/{idList}/{noList}")
-    @Operation(summary = "Get articles and contents in matrix")
-    public List<ArticleDTO> get(@PathVariable("idList") List<String> idList,
-                                @PathVariable("noList") List<Integer> noList){
-        return articleService.get(idList, noList);
+
+    @GetMapping("/{id}/{no}")
+    @Operation(summary = "Get a article")
+    public ArticleDTO get(@PathVariable("id") String id,
+                          @PathVariable("no") Integer no) {
+        return articleService.get(id, no);
     }
-//
-//    @PostMapping("/article")
-//    @Operation(summary = "Create a article with first content")
-//    public ArticleDTO createArticleWithContent(@RequestBody @Validated(ForumDTO.CreateArticleGroup.class) ForumDTO forumDTO){
-//        ContentDTO contentDTO = new ContentDTO(forumDTO.getWord());
-//        ArticleDTO articleDTO = new ArticleDTO(forumDTO.getTitle(), Collections.singletonList(contentDTO));
-//        String id = forumWriteService.createArticleWithContent(articleDTO);
-//        return forumReadService.getArticleWithContent(id, 0);
-//    }
-//
-//    @PostMapping("/article/{id}")
-//    @Operation(summary = "Create a content under article")
-//    public ArticleDTO createContent(@PathVariable("id") String id,
-//                                    @RequestBody @Validated(ForumDTO.ReplyForumGroup.class) ForumDTO forumDTO){
-//        ContentDTO contentDTO = new ContentDTO(id, forumDTO.getWord());
-//        int no = forumWriteService.createContent(contentDTO);
-//        return forumReadService.getArticleWithContent(id, no);
-//    }
-//
-//    @DeleteMapping("/article/{id}/{no}")
-//    @Operation(summary = "Delete a content. If delete first content, will also delete its article")
-//    public void deleteContent(@PathVariable("id") String id,
-//                              @PathVariable("no") int no){
-//        forumWriteService.deleteContent(id, no);
-//    }
-//
-//    @PatchMapping("/article/{id}/{no}/like")
-//    @Operation(summary = "To like a content")
-//    public void updateContentLike(@PathVariable("id") String id,
-//                                  @PathVariable("no") int no,
-//                                  @RequestBody @Validated(ForumDTO.LikeContentGroup.class) ForumDTO forumDTO){
-//        forumWriteService.updateContentLike(id, no, forumDTO.getLike());
-//    }
+
+    @PostMapping()
+    @Operation(summary = "Create a article with first content")
+    public ArticleDTO create(@RequestBody ArticleDTO articleDTO) {
+        return articleService.create(articleDTO);
+    }
+
+    @PostMapping("/{id}")
+    @Operation(summary = "Create a content under article")
+    public ArticleDTO createContent(@PathVariable("id") String id,
+                                    @RequestBody ArticleDTO articleDTO) {
+        articleDTO.setId(id);
+        return articleService.create(articleDTO);
+    }
+
+    @PatchMapping("/{id}/{no}/word")
+    @Operation(summary = "To modify a content")
+    public ArticleDTO patchWord(@PathVariable("id") String id,
+                                @PathVariable("no") int no,
+                                @RequestBody ArticleDTO articleDTO) {
+        articleDTO.setId(id);
+        articleDTO.setNo(no);
+        return articleService.patchWord(articleDTO);
+    }
+
+    @PatchMapping("/{id}/{no}/like")
+    @Operation(summary = "To like a content")
+    public ArticleDTO patchLike(@PathVariable("id") String id,
+                                @PathVariable("no") int no,
+                                @RequestBody ArticleDTO articleDTO) {
+        //TODO: need work, replace this to /like/{id}
+        return null;
+    }
+
+    @DeleteMapping("/{id}/{no}")
+    @Operation(summary = "Delete a content. If delete first content, will also delete its article")
+    public ArticleDTO patchStatusToDelete(@PathVariable("id") String id,
+                                          @PathVariable("no") int no) {
+        ArticleDTO articleDTO = new ArticleDTO(id, no);
+        articleDTO.setStatus(StatusType.DELETED);
+        return articleService.patchStatus(articleDTO);
+    }
+
 
 }
--- src/test/java/com/ag/domain/util/ObjectFieldUtilTest.java
+++ src/test/java/com/ag/domain/util/ObjectFieldUtilTest.java
@@ -1,17 +1,28 @@
 package com.ag.domain.util;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ObjectFieldUtilTest {
 
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    static class TestObject {
+        public int id;
+        public String name;
+        public String title;
+    }
+
     @Test
     void overwritePublicFields() {
         // arrange
-        TestObject target = new TestObject(10, null);
-        TestObject source = new TestObject(20, "newValue");
+        TestObject target = new TestObject(10, null, "title");
+        TestObject source = new TestObject(20, "newValue", "title");
 
         // action
         ObjectFieldUtil.overwritePublicFields(target, source);
@@ -19,17 +30,22 @@ class ObjectFieldUtilTest {
         // assert
         assertEquals(20, target.id);
         assertEquals("newValue", target.name);
+        assertEquals("title", target.title);
     }
 
+    @Test
+    void retainFields() {
+        // arrange
+        TestObject originalObject = new TestObject(1, "OriginalName", "title");
 
-    @Data
-    static class TestObject {
-        public int id;
-        public String name;
+        // action
+        TestObject retainedObject = ObjectFieldUtil.retainFields(originalObject, "title");
 
-        public TestObject(int id, String name) {
-            this.id = id;
-            this.name = name;
-        }
+        // assert
+        assertNotNull(retainedObject);
+        assertEquals(0, retainedObject.getId());
+        assertNull(retainedObject.getName());
+        assertEquals("title", retainedObject.getTitle());
     }
+
 }
