b05e410641c81ecc6f9d52621dbb17b631d4a665
alan10607 <alan10607.dev@gmail.com>
2024-03-04 19:09:20 +0800

    update article

--- src/main/java/com/ag/domain/service/ArticleService.java
+++ src/main/java/com/ag/domain/service/ArticleService.java
@@ -1,10 +1,8 @@
 package com.ag.domain.service;
 
 import com.ag.domain.constant.StatusType;
-import com.ag.domain.dto.ArticleDTO;
 import com.ag.domain.exception.AgValidationException;
 import com.ag.domain.model.Article;
-import com.ag.domain.model.Like;
 import com.ag.domain.repository.ArticleRepository;
 import com.ag.domain.service.base.CrudServiceImpl;
 import com.ag.domain.util.PojoFiledUtil;
@@ -21,67 +19,44 @@ import java.util.UUID;
 @Service
 @AllArgsConstructor
 @Slf4j
-public class ArticleService extends CrudServiceImpl<ArticleDTO> {
+public class ArticleService extends CrudServiceImpl<Article> {
     private final ArticleRepository articleRepository;
     private final LikeService likeService;
     public static final int MAX_WORD_LENGTH = 5000;
     public static final int MAX_TITLE_LENGTH = 100;
 
-    public ArticleDTO get(String id, int no) {
-        return get(new ArticleDTO(id, no));
+    public Article get(String id, int no) {
+        return get(new Article(id, no));
     }
 
-    public ArticleDTO delete(String id, int no) {
-        return delete(new ArticleDTO(id, no));
+    public Article delete(String id, int no) {
+        return delete(new Article(id, no));
     }
 
-    public ArticleDTO patchWord(ArticleDTO articleDTO) {
-        ArticleDTO patchDTO = PojoFiledUtil.retainFields(articleDTO, "id", "no", "word");
-        return patch(patchDTO);
+    public Article patchWord(Article article) {
+        Article patchedArticle = PojoFiledUtil.retainFields(article, "id", "no", "word");
+        return patch(patchedArticle);
     }
 
-    public ArticleDTO patchStatus(ArticleDTO articleDTO) {
-        ArticleDTO patchDTO = PojoFiledUtil.retainFields(articleDTO, "id", "no", "status");
-        return patch(patchDTO);
-    }
-
-    @Override
-    public ArticleDTO getImpl(ArticleDTO articleDTO) {
-        Article article = articleRepository.findById(articleDTO.getId()).get();
-        return returnFilter(article);
-    }
-
-    private ArticleDTO returnFilter(Article article) {
-        ArticleDTO articleDTO = PojoFiledUtil.convertObject(article, ArticleDTO.class);
-        switch (articleDTO.getStatus()) {
-            case NORMAL:
-                //TODO: prepare after get
-                Like like = likeService.get(articleDTO.getId(), articleDTO.getNo(), "userId");
-                articleDTO.setLike(like.getState());
-
-//                UserDTO userDTO = userService.get(contentDTO.getAuthorId());
-//                contentDTO.setAuthorName(userDTO.getUsername());
-//                contentDTO.setAuthorHeadUrl(userDTO.getHeadUrl());
-//                contentDTO.setLike(likeService.get(contentDTO.getId(), contentDTO.getNo(), AuthUtil.getUserId()));
-                return articleDTO;
-            case DELETED:
-                return new ArticleDTO(articleDTO.getId(), articleDTO.getNo(), StatusType.DELETED);
-            case UNKNOWN:
-            default:
-                log.info("Article {}/{} not found", articleDTO.getId(), articleDTO.getNo());
-                return new ArticleDTO(articleDTO.getId(), articleDTO.getNo(), StatusType.DELETED);
-        }
+    public Article patchStatus(Article article) {
+        Article patchedArticle = PojoFiledUtil.retainFields(article, "id", "no", "status");
+        return patch(patchedArticle);
     }
 
     @Override
-    public ArticleDTO createImpl(ArticleDTO articleDTO) {
+    public Article getImpl(Article article) {
+        return articleRepository.findById(article.getId()).orElse(null);
+    }
+
+    @Override
+    public Article createImpl(Article article) {
         LocalDateTime now = TimeUtil.now();
-        Article article = Article.builder()
+        article = Article.builder()
                 .id(UUID.randomUUID().toString())
                 .no(0)
                 .authorId(AuthUtil.getUserId())
-                .title(articleDTO.getTitle())
-                .word(articleDTO.getWord())
+                .title(article.getTitle())
+                .word(article.getWord())
                 .likes(0L)
                 .status(StatusType.NORMAL)
                 .createDate(now)
@@ -89,92 +64,95 @@ public class ArticleService extends CrudServiceImpl<ArticleDTO> {
                 .build();
 
 
-        article = articleRepository.save(article);
-        return returnFilter(article);
+        return articleRepository.save(article);
     }
 
     @Override
-    public ArticleDTO updateImpl(ArticleDTO articleDTO) {
+    public Article updateImpl(Article article) {
         LocalDateTime now = TimeUtil.now();
-        Article article = Article.builder()
-                .id(articleDTO.getId())
-                .no(articleDTO.getNo())
-                .authorId(articleDTO.getAuthorId())
-                .title(articleDTO.getTitle())
-                .word(articleDTO.getWord())
-                .likes(articleDTO.getLikes())
-                .status(articleDTO.getStatus())
-                .createDate(articleDTO.getCreateDate())
+        article = Article.builder()
+                .id(article.getId())
+                .no(article.getNo())
+                .authorId(article.getAuthorId())
+                .title(article.getTitle())
+                .word(article.getWord())
+                .likes(article.getLikes())
+                .status(article.getStatus())
+                .createDate(article.getCreateDate())
                 .updateDate(now)
                 .build();
 
-        article = articleRepository.save(article);
-        return returnFilter(article);
+        return articleRepository.save(article);
     }
 
     @Override
-    public ArticleDTO patchImpl(ArticleDTO articleDTO) {
-        return update(articleDTO);
+    public Article patchImpl(Article article) {
+        return update(article);
     }
 
     @Override
-    public ArticleDTO deleteImpl(ArticleDTO articleDTO) {
-        Article article = PojoFiledUtil.convertObject(articleDTO, Article.class);
-        return  null;//returnFilter(articleRepository.delete(article));
+    public Article deleteImpl(Article article) {
+        articleRepository.delete(article);
+        return article;
     }
 
     @Override
-    protected void beforeGet(ArticleDTO articleDTO) {
-        validateFirstArticleIsNormal(articleDTO);
+    protected void beforeGet(Article article) {
+        validateFirstArticleIsNormal(article);
     }
 
     @Override
-    protected void beforeCreate(ArticleDTO articleDTO) {
-        validateTitle(articleDTO);
-        validateWord(articleDTO);
+    protected void beforeCreate(Article article) {
+        validateTitle(article);
+        validateWord(article);
     }
 
     @Override
-    protected void beforeUpdateAndPatch(ArticleDTO articleDTO) {
-        validateWord(articleDTO);
-        validateStatus(articleDTO);
+    protected void beforeUpdateAndPatch(Article article) {
+        validateHavePermission(article);
+        validateWord(article);
+        validateStatus(article);
     }
 
     @Override
-    protected void beforeDelete(ArticleDTO articleDTO) {
-        validateStatus(articleDTO);
+    protected void beforeDelete(Article article) {
+        validateStatus(article);
+        validateHavePermission(article);
     }
 
-    void validateWord(ArticleDTO articleDTO) {
-        if (StringUtils.isBlank(articleDTO.getWord())) {
-            throw new AgValidationException("Word must not be blank", articleDTO);
+    void validateWord(Article article) {
+        if (StringUtils.isBlank(article.getWord())) {
+            throw new AgValidationException("Word must not be blank", article);
         }//TODO: please update front end
-        if (articleDTO.getWord().getBytes().length > MAX_WORD_LENGTH) {
-            throw new AgValidationException("Word length must in {} bytes", articleDTO, MAX_WORD_LENGTH);
+        if (article.getWord().getBytes().length > MAX_WORD_LENGTH) {
+            throw new AgValidationException("Word length must in {} bytes", article, MAX_WORD_LENGTH);
         }
     }
 
-    void validateTitle(ArticleDTO articleDTO) {
-        if (articleDTO.getNo() == 0 && StringUtils.isBlank(articleDTO.getTitle())) {
-            throw new AgValidationException("Title must not be blank when create", articleDTO);
+    void validateTitle(Article article) {
+        if (article.getNo() == 0 && StringUtils.isBlank(article.getTitle())) {
+            throw new AgValidationException("Title must not be blank when create", article);
         }
-        if (articleDTO.getTitle().getBytes().length > MAX_TITLE_LENGTH) {
-            throw new AgValidationException("Title length must in {} bytes", articleDTO, MAX_TITLE_LENGTH);
-        }
-    }
-
-    void validateStatus(ArticleDTO articleDTO) {
-        if (articleDTO.getStatus() != StatusType.NORMAL) {
-            throw new AgValidationException("Status is not normal", articleDTO);
+        if (article.getTitle().getBytes().length > MAX_TITLE_LENGTH) {
+            throw new AgValidationException("Title length must in {} bytes", article, MAX_TITLE_LENGTH);
         }
     }
 
-    void validateFirstArticleIsNormal(ArticleDTO articleDTO) {
-//        Article firstArticle = articleRepository.get(articleDTO.getId(), 0);
+    void validateStatus(Article article) {
+        if (article.getStatus() != StatusType.NORMAL) {
+            throw new AgValidationException("Status is not normal", article);
+        }
+    }
+
+    void validateFirstArticleIsNormal(Article article) {
+//        Article firstArticle = articleRepository.get(article.getId(), 0);
 //        if (firstArticle.getStatus() != StatusType.NORMAL) {
-//            throw new AgValidationException("First article's status is not normal", articleDTO);
+//            throw new AgValidationException("First article's status is not normal", article);
 //        }
     }
 
+    void validateHavePermission(Article article) {
+
+    }
 
 }
