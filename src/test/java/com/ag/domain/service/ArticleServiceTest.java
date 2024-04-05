7ee0fdfa7810e609b703ab1095d269223e19e6d2
alan10607 <alan10607.dev@gmail.com>
2024-04-05 17:31:47 +0800

    article unit test

--- src/test/java/com/ag/domain/service/ArticleServiceTest.java
+++ src/test/java/com/ag/domain/service/ArticleServiceTest.java
@@ -1,175 +1,183 @@
-//package com.ag.domain.service;
-//
-//import com.ag.domain.constant.StatusType;
-//import com.ag.domain.dto.ArticleDTO;
-//import com.ag.domain.exception.AgValidationException;
-//import com.ag.domain.model.Article;
-//import com.ag.domain.repository.ArticleRepository;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.junit.runner.RunWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.MockitoJUnitRunner;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//import java.util.Random;
-//
-//import static com.ag.domain.service.ArticleService.MAX_TITLE_LENGTH;
-//import static com.ag.domain.service.ArticleService.MAX_WORD_LENGTH;
-//import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.mockito.ArgumentMatchers.*;
-//import static org.mockito.Mockito.when;
-//
-//@RunWith(MockitoJUnitRunner.class)
-//@ExtendWith(MockitoExtension.class)
-//class ArticleServiceTest {
-//
-//    @InjectMocks
-//    private ArticleService articleService;
-//    @Mock
-//    private ArticleRepository articleRepository;
-//
-//    private static final ArticleDTO article = new ArticleDTO();
-//    private static final Article deletedAllArticle = new Article();
-//
-//    @BeforeAll
-//    public static void setUp() {
-//        article.setId("123d3489-ef6c-43ab-b86f-099405793e58");
-//        article.setNo(0);
-//
-//        deletedAllArticle.setId("97393044-eb88-4a1e-be2e-7552825627a0");
-//        deletedAllArticle.setNo(0);
-//        deletedAllArticle.setStatus(StatusType.DELETED);
-//    }
-//
-//
-//    @Test
-//    public void validateWord_should_failed_if_is_blank() {
-//        // Arrange
-//        article.setWord("");
-//
-//        // Act & Assert
-//        assertThrows(AgValidationException.class, () -> {
-//            articleService.validateWord(article);
-//        });
-//
-//        // Arrange
-//        article.setWord(null);
-//
-//        // Act & Assert
-//        assertThrows(AgValidationException.class, () -> {
-//            articleService.validateWord(article);
-//        });
-//
-//    }
-//
-//    @Test
-//    public void validateWord_should_success_if_length_in_max_bytes() {
-//        // Arrange
-//        article.setWord(generateRandomString(MAX_WORD_LENGTH));
-//
-//        // Act & Assert
-//        assertDoesNotThrow(() -> {
-//            articleService.validateWord(article);
-//        });
-//    }
-//
-//    @Test
-//    public void validateWord_should_failed_if_length_exceeds_max_bytes() {
-//        // Arrange
-//        article.setWord(generateRandomString(MAX_WORD_LENGTH + 1));
-//
-//        // Act & Assert
-//        assertThrows(AgValidationException.class, () -> {
-//            articleService.validateWord(article);
-//        });
-//    }
-//
-//    @Test
-//    public void validateTitle_should_failed_if_is_blank() {
-//        // Arrange
-//        article.setTitle("");
-//
-//        // Act & Assert
-//        assertThrows(AgValidationException.class, () -> {
-//            articleService.validateTitle(article);
-//        });
-//
-//        // Arrange
-//        article.setTitle(null);
-//
-//        // Act & Assert
-//        assertThrows(AgValidationException.class, () -> {
-//            articleService.validateTitle(article);
-//        });
-//
-//    }
-//
-//    @Test
-//    public void validateTitle_should_success_if_length_in_max_bytes() {
-//        // Arrange
-//        article.setTitle(generateRandomString(MAX_TITLE_LENGTH));
-//
-//        // Act & Assert
-//        assertDoesNotThrow(() -> {
-//            articleService.validateTitle(article);
-//        });
-//    }
-//
-//    @Test
-//    public void validateTitle_should_failed_if_length_exceeds_max_bytes() {
-//        // Arrange
-//        article.setTitle(generateRandomString(MAX_TITLE_LENGTH + 1));
-//
-//        // Act & Assert
-//        assertThrows(AgValidationException.class, () -> {
-//            articleService.validateTitle(article);
-//        });
-//    }
-//
-//    @Test
-//    public void validateStatus_should_failed_if_status_is_not_normal() {
-//        // Arrange
-//        ArticleDTO articleDTO = new ArticleDTO();
-//        articleDTO.setId();
-//        articleDTO.setNo(0);
-//        articleDTO.setStatus(StatusType.DELETED);
-//
-//        // Act & Assert
-//        assertThrows(AgValidationException.class, () -> {
-//            articleService.validateStatus(articleDTO);
-//        });
-//    }
-//
-//    @Test
-//    public void validateFirstArticleIsNormal_should_failed_if_first_article_status_is_not_normal() {
-//        // Arrange
-//        ArticleDTO articleDTO = new ArticleDTO();
-//        articleDTO.setId(deletedAllArticle.getId());
-//        articleDTO.setNo(2);
-//        when(articleRepository.get(anyString(), anyInt())).thenReturn(deletedAllArticle);
-//
-//        // Act & Assert
-//        assertThrows(AgValidationException.class, () -> {
-//            articleService.validateFirstArticleIsNormal(articleDTO);
-//        });
-//
-//    }
-//
-//    private static String generateRandomString(int length) {
-//        Random random = new Random();
-//        StringBuilder sb = new StringBuilder(length);
-//        for (int i = 0; i < length; ++i) {
-//            char randomChar = (char) (random.nextInt(26) + 'a');
-//            sb.append(randomChar);
-//        }
-//        return sb.toString();
-//    }
-//
-//    private Article generateArticle(){
-//
-//    }
-//}
+package com.ag.domain.service;
+
+import com.ag.domain.constant.ArticleStatus;
+import com.ag.domain.exception.AgValidationException;
+import com.ag.domain.model.Article;
+import com.ag.domain.repository.ArticleRepository;
+import com.ag.domain.util.AuthUtil;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.ag.domain.TestUtil.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
+class ArticleServiceTest {
+
+    @InjectMocks
+    private ArticleService articleService;
+
+    @Mock
+    private ArticleRepository articleRepository;
+
+    @BeforeAll
+    public static void setup(){
+        mockStatic(AuthUtil.class);
+    }
+
+    @Test
+    void validateArticleId_should_success_if_is_uuid() {
+        // Arrange
+        Article article = generateArticle(UUID.randomUUID().toString(), 0);
+
+        // Act & Assert
+        assertDoesNotThrow(() -> articleService.validateArticleId(article));
+    }
+
+    @Test
+    void validateArticleId_should_failed_if_not_uuid() {
+        // Arrange
+        Article article = generateArticle("1234", 0);
+
+        // Act & Assert
+        assertThrows(AgValidationException.class, () -> articleService.validateArticleId(article));
+    }
+
+    @Test
+    void validateNo_should_success_if_no_is_greater_than_or_equal_to_zero() {
+        // Arrange
+        Article article = generateArticle(UUID.randomUUID().toString(), 0);
+
+        // Act & Assert
+        assertDoesNotThrow(() -> articleService.validateNo(article));
+    }
+
+    @Test
+    void validateNo_should_failed_if_no_is_less_than_zero() {
+        // Arrange
+        Article article = generateArticle(UUID.randomUUID().toString(), -1);
+
+        // Act & Assert
+        assertThrows(AgValidationException.class, () -> articleService.validateNo(article));
+    }
+
+    @Test
+    void validateFirstArticleStatusIsNormal_should_succeed_when_status_is_normal() {
+        // Arrange
+        Article article = generateArticle();
+        article.setStatus(ArticleStatus.NORMAL);
+        when(articleRepository.findById(article.getId())).thenReturn(Optional.of(article));
+
+        // Act & Assert
+        assertDoesNotThrow(() -> articleService.validateFirstArticleStatusIsNormal(article));
+    }
+
+    @Test
+    void validateFirstArticleStatusIsNormal_should_failed_when_status_is_not_normal() {
+        // Arrange
+        Article article = generateArticle();
+        article.setStatus(ArticleStatus.DELETED);
+        when(articleRepository.findById(article.getId())).thenReturn(Optional.of(article));
+
+        // Act & Assert
+        assertThrows(AgValidationException.class, () -> articleService.validateFirstArticleStatusIsNormal(article));
+    }
+
+    @Test
+    void validateWord_should_succeed_when_word_length_is_within_max_limit() {
+        // Arrange
+        Article article = generateArticle();
+        article.setWord(generateRandomString(ArticleService.MAX_WORD_LENGTH));
+
+        // Act & Assert
+        assertDoesNotThrow(() -> articleService.validateWord(article));
+    }
+
+    @Test
+    void validateWord_should_failed_when_word_length_exceeds_max_limit() {
+        // Arrange
+        Article article = generateArticle();
+        article.setWord(generateRandomString(ArticleService.MAX_WORD_LENGTH + 1));
+
+        // Act & Assert
+        assertThrows(AgValidationException.class, () -> articleService.validateWord(article));
+    }
+
+    @Test
+    void validateTitle_should_succeed_when_is_first_article_and_title_length_is_within_max_limit() {
+        // Arrange
+        Article article = generateArticle();
+        article.setCreatingFirstArticle();
+        article.setTitle(generateRandomString(ArticleService.MAX_TITLE_LENGTH));
+
+        // Act & Assert
+        assertDoesNotThrow(() -> articleService.validateTitle(article));
+    }
+
+    @Test
+    void validateTitle_should_failed_when_is_first_article_and_title_length_exceeds_max_limit() {
+        // Arrange
+        Article article = generateArticle();
+        article.setCreatingFirstArticle();
+        article.setTitle(generateRandomString(ArticleService.MAX_TITLE_LENGTH + 1));
+
+        // Act & Assert
+        assertThrows(AgValidationException.class, () -> articleService.validateTitle(article));
+    }
+
+    @Test
+    void validateTitle_should_succeed_when_is_replying_article_and_title_is_null() {
+        // Arrange
+        Article article = generateArticle();
+        article.setCreatingReplyArticle(UUID.randomUUID().toString());
+        article.setTitle(null);
+
+        // Act & Assert
+        assertDoesNotThrow(() -> articleService.validateTitle(article));
+    }
+
+    @Test
+    void validateTitle_should_failed_when_is_replying_article_and_title_is_not_null() {
+        // Arrange
+        Article article = generateArticle();
+        article.setCreatingReplyArticle(UUID.randomUUID().toString());
+        article.setTitle("have title");
+
+        // Act & Assert
+        assertThrows(AgValidationException.class, () -> articleService.validateTitle(article));
+    }
+
+    @Test
+    void validateHavePermission_should_succeed_when_user_is_author() {
+        // Arrange
+        Article article = generateArticle();
+        when(AuthUtil.getUser()).thenReturn(generateUser(article.getAuthorId()));
+
+        // Act & Assert
+        assertDoesNotThrow(() -> articleService.validateHavePermission(article));
+    }
+
+    @Test
+    void validateHavePermission_should_failed_when_user_is_not_author() {
+        // Arrange
+        Article article = generateArticle();
+        when(AuthUtil.getUser()).thenReturn(generateUser(UUID.randomUUID().toString()));
+
+        // Act & Assert
+        assertThrows(AgValidationException.class, () -> articleService.validateHavePermission(article));
+    }
+
+}
